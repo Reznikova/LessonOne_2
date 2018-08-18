9f908e905b7ed8638cff31dc89912ca20b4c6164
Tom <tom@mail.ru>
2018-08-18 19:29:08 +0300

    initial commit

--- src/org/itstep/qa/control/clk/TestOne.java
+++ src/org/itstep/qa/control/clk/TestOne.java
@@ -1,11 +1,17 @@
 package org.itstep.qa.control.clk;
 
+import static java.lang.System.out;
+
 public class TestOne {
     public static void main (String[] args) {
         int testVar;
         testVar = 3;
         int testTwo;
         testTwo = (2 + testVar) * 3;
-        System.out.println(testTwo);
+        out.println(testTwo);
+
+        out.println(testTwo);
+
+        out.println(testTwo);
     }
 }
